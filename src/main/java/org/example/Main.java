342c88398e2268abdf8c9a8babc7a2e9ee340a51
EkaterinaFefelova <fefelova.ekaterina@gmail.com>
2024-09-08 16:40:32 +0300

    HW40 complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,44 @@
 package org.example;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        String dbUrl = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String pass = "Gibsonsg19!";
 
+        try {
+            Connection connection = DriverManager.getConnection(dbUrl, user, pass);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM instructors");
+            List<Instructor> instructors = new ArrayList<>();
+            while(resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int salary = resultSet.getInt("salary");
+                int age = resultSet.getInt("age");
+                instructors.add(new Instructor(id, name, salary, age));
+            }
+            instructors.forEach(System.out::println);
+
+            /*ResultSet resultSet = statement.executeQuery("SELECT * FROM cars");
+            List<Car> cars = new ArrayList<>();
+            while(resultSet.next()){
+                int id = resultSet.getInt ("id");
+                String name = resultSet.getString("name");
+                int mileage = resultSet.getInt("mileage");
+                cars.add(new Car(id, name, mileage));
+            }
+            cars.forEach(System.out::println);*/
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
